1ee74363130b4416b84f8830dd07333447ad5ceb
ljm_glb <3231055159_glb@qq.com>
2024-07-22 12:50:02 +0800

    02_python gs2

---
+++ python_study/python_project/02_python语言概述/03_comment.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+# 注释语句
+print('hello,jack')
+print('hello,jack')
+print('hello,jack')
+print('hello,jack')
+print('hello,jack')
+# print('hello,jack')
+# print('hello,tom')
+# print('hello,mary')
+'''print('hello,hsp')
+print('hello,smith')
+'''
