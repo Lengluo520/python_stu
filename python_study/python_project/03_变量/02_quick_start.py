bb62190a4c2b75376256671e63e538007aa0c76b
Lengluo520 <3231055159@qq.com>
2024-07-23 20:25:54 +0800

    02_quick_start.py

---
+++ python_study/python_project/03_变量/02_quick_start.py
@@ -0,0 +1,15 @@
+name = 'ljm'    #string 字符串
+age = 22        #int   整型
+score = 90.4    #浮点型  float
+gender = '男'   #字符串
+print('-------------------------------')
+print('人员信息如下：')
+print(name)
+print(age)
+print(score)
+print(gender)
+print('-------------------------------')
+print('人员信息如下：',name,age,score,gender)
+print('-------------------------------')
+print('人员信息如下：','\n',name,age,score,gender)
+print('-------------------------------')
