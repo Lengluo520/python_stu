e5f73456b86a1459c99e1516a62608ff432a93f2
Lengluo520 <3231055159@qq.com>
2024-07-28 09:05:12 +0800

    str

---
+++ python_study/python_project/03_变量/09_str_detail.py
@@ -0,0 +1,14 @@
+# 字符串使用
+str1 = 'tom说:"hello"'
+print(str1)
+str2 = "tom说:'hi'"
+print(str2)
+
+print(str1+str2)
+
+content ='''dfjalfj''""<>>>>=-='''
+print(content)
+
+#在字符串前加'r',可以使整个字符串不会转义
+str4 = r'jack\ntom\tking'
+print(str4)
