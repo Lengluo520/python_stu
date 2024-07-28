e57cdef7262d02fd6428c411c7e00b733731ec14
Lengluo520 <3231055159@qq.com>
2024-07-28 09:04:49 +0800

    bool

---
+++ python_study/python_project/03_变量/08_bool_detail.py
@@ -0,0 +1,20 @@
+#bool类型的基本使用
+
+num1 = 100
+num2 = 200
+if num1 > num2:
+    print('num1 > num2')
+else:
+    print('num1 < num2')
+result = num1 > num2
+print('result = ',result)
+
+b1 = False
+b2 = True
+print(b1 + 10)
+print(b2 + 10)
+
+if 1.3:
+    print('哈哈哈')
+if 0:
+    print('嘻嘻')
