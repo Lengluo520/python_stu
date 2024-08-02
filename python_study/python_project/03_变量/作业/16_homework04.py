2dd0dd6ec6536f767764a4caf4e8f4ff848702d1
Lengluo520 <3231055159@qq.com>
2024-08-02 09:54:41 +0800

    16

---
+++ python_study/python_project/03_变量/作业/16_homework04.py
@@ -0,0 +1,11 @@
+name = 'ljm'
+age = '22'
+score = 90.2
+gender = '男'
+hobby = '计算机'
+
+#用一行输出
+print('姓名\t\t年龄\t\t成绩\t\t性别''\t\t爱好\n'
+      +name+'\t\t'+str(age)+'\t\t'+str(score)
+      +'\t'+gender+'\t\t'+hobby)
+
