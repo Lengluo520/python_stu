9cb5fd6dc63d118cbf07f6cb9fa676e27ffe1e0a
Lengluo520 <3231055159@qq.com>
2024-08-03 09:08:57 +0800

    compare

---
+++ python_study/python_project/04_运算符/03_compare_operator.py
@@ -0,0 +1,13 @@
+# 比较运算符使用
+a = 9
+b = 8
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(a < b)
+print(a == b)
+print(a != b)
+flag = a > b
+print('flag = ', flag)
+print(a is b)
+print(a is not b)
