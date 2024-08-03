c5a26affab160a34ef303da716e5f92efe1f9931
Lengluo520 <3231055159@qq.com>
2024-08-03 09:08:16 +0800

    airthmetic

---
+++ python_study/python_project/04_运算符/01_arithmetic_operator.py
@@ -0,0 +1,11 @@
+# 算数运算符的使用
+print(10 / 3)
+print(10 // 3)
+print(-9 // 2)
+print(-10 // 3)
+print(10 % 3)  # 求摸公式  a % b = a - a // b * b
+print(-10 % 3)
+print(10 % -3)
+print(-10 % -3)
+print(2 ** 5)
+print(9 ** 2)
