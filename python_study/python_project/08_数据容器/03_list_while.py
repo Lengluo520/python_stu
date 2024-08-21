f9350a15d1068554b451b32ea394e7c25cbb0fa1
Lengluo520 <3231055159@qq.com>
2024-08-21 12:30:00 +0800

    while

---
+++ python_study/python_project/08_数据容器/03_list_while.py
@@ -0,0 +1,7 @@
+#列表的遍历
+list_color = ['red', 'green', 'blue','yellow','white','black']
+print(len(list_color))
+index = 0
+while index < len(list_color):
+    print(f'第{index + 1}个元素的是：{list_color[index]}')
+    index += 1
