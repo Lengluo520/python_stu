7505e719f5243ea9e1981f395a91ebe53c6779fe
Lengluo520 <3231055159@qq.com>
2024-08-25 16:45:26 +0800

    detail

---
+++ python_study/python_project/08_数据容器/11_tuple_detail.py
@@ -0,0 +1,10 @@
+tuple_a = ()
+tuple_b = tuple()
+print(f'tuple_a : {tuple_a}{type(tuple_a)}')
+print(f'tuple_b : {tuple_b}{type(tuple_b)}')
+
+tuple_c = (100,'jack',4.5,True,'jack',('tom',15))
+print(f'tuple_c : {tuple_c}{type(tuple_c)}')
+
+tuple_f = (1,2,3,'你好',['jack','tom','ok'])
+print(tuple_f[4][1]) 
