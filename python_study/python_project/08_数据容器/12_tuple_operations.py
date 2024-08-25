2b7cc3016706dbcb1fcae43eafe19921523a0a66
Lengluo520 <3231055159@qq.com>
2024-08-25 16:45:45 +0800

    operation

---
+++ python_study/python_project/08_数据容器/12_tuple_operations.py
@@ -0,0 +1,10 @@
+tuple_a =(100,200,300,400,500)
+print('tuple_a 元组的个数：',len(tuple_a))
+print('tuple_a 元组最大元素',max(tuple_a))
+print('tuple_a 元组最小的元素',min(tuple_a))
+print('100出现了次数是:',tuple_a.count(100))
+print('200第一次出现在元组中的索引是：',tuple_a.index(200))
+
+print('*'*30)
+
+print(ord('刘'))
