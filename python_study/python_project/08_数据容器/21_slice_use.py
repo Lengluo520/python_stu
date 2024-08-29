ff8d97f3a216d096be738477b89a9c4b397a2b86
Lengluo520 <3231055159@qq.com>
2024-08-29 14:16:19 +0800

    use

---
+++ python_study/python_project/08_数据容器/21_slice_use.py
@@ -0,0 +1,11 @@
+str = 'hello world'
+str_slice = str[0:5]
+print(str_slice)
+
+list_a = ['jack','tom','yoyo','nono','hsp']
+list_slice = list_a[1:4:2]
+print(list_slice)
+
+tuple_a = (100,200,300, 400, 500,600)
+tuple_slice = tuple_a[1:5:1]
+print(tuple_slice)
