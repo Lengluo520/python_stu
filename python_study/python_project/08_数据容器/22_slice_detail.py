fd87b9c75ba39d60bef49d95b348b5e0944faa77
Lengluo520 <3231055159@qq.com>
2024-08-30 10:02:28 +0800

    detail

---
+++ python_study/python_project/08_数据容器/22_slice_detail.py
@@ -0,0 +1,11 @@
+str = '123456'
+str_slice05 = str[-1::-1]
+print(str_slice05)
+
+str_slice06 = str[-1:-6:-2]
+print(str_slice06)
+
+str = 'ABCD'
+str_slice07 = str[1:3:1]
+print(str)
+print(str_slice07)
