56a4573a648f6791dd29ff8251eb5d230a71dad4
Lengluo520 <3231055159@qq.com>
2024-09-04 10:44:07 +0800

    convert

---
+++ python_study/python_project/08_数据容器/31_container_convert.py
@@ -0,0 +1,28 @@
+str_a ='hello'
+list_a =['jack','tom','mary']
+tuple_a =('hsp','tim')
+set_a = {'red','black'}
+dict_a = {'0001':'小倩','0002':'黑山老妖'}
+
+print(f'str_a转成list: {list(str_a)}')
+print(f'tuple_a转成list:{list(tuple_a)}')
+print(f'set_a转成list:{list(set_a)}')
+print(f'dict_a转成list:{list(dict_a)}')
+
+print('='*30)
+print(f'str_a转成str: {str(str_a)}')
+print(f'tuple_a转成str:{str(tuple_a)}')
+print(f'set_a转成str:{str(set_a)}')
+print(f'dict_a转成str:{str(dict_a)}')
+
+print('='*30)
+print(f'str_a转成tuple: {tuple(str_a)}')
+print(f'tuple_a转成tuple:{tuple(tuple_a)}')
+print(f'set_a转成tuple:{tuple(set_a)}')
+print(f'dict_a转成tuple:{tuple(dict_a)}')
+
+print('='*30)
+print(f'str_a转成set: {set(str_a)}')
+print(f'tuple_a转成set:{set(tuple_a)}')
+print(f'set_a转成set:{set(set_a)}')
+print(f'dict_a转成set:{set(dict_a)}')
